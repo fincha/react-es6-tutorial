4dcae1cb8b66acb31c71577033ca34c500583887
Alexander Frischbutter <alexander.frischbutter@ing-diba.de>
2017-05-15 16:10:02 +0200

    init commit

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+    entry: './js/app.js',
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'app.bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            }
+        ]
+    },
+    stats: {
+        colors: true
+    },
+    devtool: 'source-map'
+};/**
+ * Created by arfrischbutter on 15.05.17.
+ */
